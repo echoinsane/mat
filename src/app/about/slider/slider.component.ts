2a266781a5fa947fc7c5f6d7709fc3add0df46f8
echoinsane <echoinsane@mail.ru>
2017-12-17 04:28:17 +0300

    added first logic for slider

--- src/app/about/slider/slider.component.ts
+++ src/app/about/slider/slider.component.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { persons} from './persons';
+
+import { IPerson } from '../../../model/IPerson';
 
 @Component({
 	selector: 'slider',
@@ -6,4 +10,37 @@ import {Component} from '@angular/core';
 	styleUrls: ['./slider.style.less'],
 })
 export class SliderComponent {
+	@ViewChild('gallery') gallery: ElementRef;
+	persons: IPerson[] = persons;
+	private currentPerson = [1, 2, 3];
+
+	left() {
+		const lastId = this.currentPerson[this.currentPerson.length - 1];
+		const firstId = this.currentPerson[0];
+
+		if (firstId === this.persons[0].id) {
+			this.persons.unshift(this.persons[this.persons.length - 1]);
+			this.currentPerson.unshift(this.persons[this.persons.length - 1].id);
+			this.currentPerson.pop();
+
+			// this.gallery.nativeElement.style({transform: 'translateX(-350px)'});
+			this.persons.pop();
+		}
+
+
+	}
+
+	right() {
+		const lastId = this.currentPerson[this.currentPerson.length - 1];
+		const firstId = this.currentPerson[0];
+
+		if (lastId === this.persons[this.persons.length - 1].id) {
+			this.persons.push(this.persons[0]);
+			this.currentPerson.push(this.persons[0].id);
+			this.currentPerson.shift();
+
+			// this.gallery.nativeElement.style({transform: 'translateX(-350px)'});
+			this.persons.shift();
+		}
+	}
 }
